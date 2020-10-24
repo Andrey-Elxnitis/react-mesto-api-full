5702d4fb4f713ca9481142a1c75340ede1f62140
Andrey Elxnitis <elxnitisandrei@yandex.ru>
2020-10-24 10:59:33 +0300

    Исправление

--- controllers/users.js
+++ controllers/users.js
@@ -65,12 +65,7 @@ const login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: true,
-      })
-        .send({ message: 'Авторизация прошла успешно' });
+      res.status(200).send({ token });
     })
     .catch(next);
 };
